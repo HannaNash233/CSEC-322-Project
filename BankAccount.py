1364b07cc67a597d6a93b7a401cc2c0a5e7ecf37
victoriaseusankar <victoriaseusankar@go.rmc.edu>
2025-10-06 10:48:53 -0400

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -5,3 +5,13 @@
 # Date: 9/6/25
 
 # This module defines the BankAccount class.
+
+class BankAccount:
+  # Constructs a bank account.
+  @param firstName: The first name of the account holder
+  @param lastName: The last name of the account holder
+  @param initBalance: The starting bank account balance
+  def __init__(firstName, lastName, initBalance = 0.0):
+    pass
+
+
