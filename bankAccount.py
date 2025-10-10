9d076787a4e5df26caf36b4c0be3983b55abbf55
detricb1 <detricbrown@go.rmc.edu>
2025-10-10 08:55:26 -0400

    bankAccount.py

--- bankAccount.py
+++ bankAccount.py
@@ -22,7 +22,7 @@ class BankAccount:
   #@require firstName to be 1-25 characters with no special characters
   #@require lastName to be 1-40 characters with no special characters
   #@require accountNumber is greater than or equal to 1000
-  def __init__(firstName, lastName, initBalance = 0.0):
+  def __init__(self, firstName, lastName, initBalance = 0.0):
 
     # Assertions
     assert firstName.isalpha and firstName >= 1 and firstName <= 25
