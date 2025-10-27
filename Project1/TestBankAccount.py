4e23659761cbf94740583f4bf9efd19600039d84
HannaNash233 <145599518+HannaNash233@users.noreply.github.com>
2025-10-27 08:05:52 -0400

    Add files via upload

---
+++ Project1/TestBankAccount.py
@@ -0,0 +1,191 @@
+# TestBankAccount.py
+# 
+# Authors: Hanna Nash, Victoria Seusankar, Will Wian, Detric Brown
+# 
+# Date: 9/6/25
+
+# This file tests the functions in the BankAccount.py file
+
+import unittest
+from bankAccount import BankAccount
+
+class TestBankAccount(unittest.TestCase):
+  # Constants used for testing
+    INITIAL_BALANCE = 1000.00
+    DEPOSIT_AMOUNT = 500.00
+    WITHDRAW_AMOUNT = 200.00
+    OVERDRAFT_LIMIT = 250.00
+    DEBUG = True
+
+    BALANCE1 = 3200
+    WITHDRAWL1 = 500
+    DEPOSIT1 = 450
+    FIRST_NAME1 = "Hank"
+    LAST_NAME1 = "Hill"
+    
+    BALANCE2 = 1500
+    WITHDRAWL2 = 1750
+    DEPOSIT2 = 200
+    FIRST_NAME2 = "Mickey"
+    LAST_NAME2 = "Mouse"    
+
+  # The setup method creates bank accounts.
+    def setUp(self):
+        self.bankAccount1 = BankAccount("John", "Smith", TestBankAccount.INITIAL_BALANCE)
+        self.bankAccount2 = BankAccount("Nancy", "Jackson", TestBankAccount.INITIAL_BALANCE)
+        self.bankAccount3 = BankAccount("Reggie", "Miller", 50)
+        self.acc1 = BankAccount("William", "Wian", 1000000)
+        self.account4 = BankAccount(TestBankAccount.FIRST_NAME1, TestBankAccount.LAST_NAME1, TestBankAccount.BALANCE1)
+        self.account5 = BankAccount(TestBankAccount.FIRST_NAME2, TestBankAccount.LAST_NAME2, TestBankAccount.BALANCE2)
+        
+        # checking that the asserts fail within the account creation
+        try:
+            self.account6 = BankAccount("D", "P", TestBankAccount.BALANCE1)
+            self.account7 = BankAccount("MabelAndDipperPines", "StanfordAndStanleyPines", TestBankAccount.BALANCE1)
+        except AssertionError as error:
+            print(error)
+                
+
+  
+  # The test_constructor method tests the constructor.
+    def test_constructor(self):
+        self.assertEqual(self.account4.getFirst(), "Hank")
+        self.assertEqual(self.account4.getLast(), "Hill")
+        self.assertEqual(self.account4.getBalance(), TestBankAccount.BALANCE1)
+        if TestBankAccount.debug:
+            print("\nTesting Constructor")
+            print("The First Account: ", self.account4)
+            print("The Second Account: ", self.account5)  
+   
+
+
+  # Test to see if the transfer is successful
+    def testTransferSuccessful(self):
+        # Print debug message if DEBUG is True
+        if TestBankAccount.DEBUG:
+            print("\nTesting a successful transfer:")
+
+        # Define the amount being transferred
+        transferAmount = 100.00
+        
+        # self.bankAccount1 is the receiver
+        # self.bankAccount2 is the source sender
+        # Attempt to transfer money from bankAccount2 to bankAccount1
+        success = self.bankAccount1.tranfer(self.bankAccount2, transferAmount)
+
+        # Assert that the transfer function is successful 
+        self.assertTrue(success)
+
+        # Assert that bankAccount1's balance increased after the transfer; it should equal the initial balance plus the transferred amount
+        self.assertEqual(self.bankAccount1.balance, TestBankAccount.INITIAL_BALANCE + transferAmount)
+
+        # Assert that bankAccount2's balance decreased after the transfer; it should equal the initial balance minus the transferred amount
+        self.assertEqual(self.bankAccount2.balance, TestBankAccount.INITIAL_BALANCE - transferAmount)
+
+
+   # Test to see if the transfer is denied 
+    def testTransferDenied(self):
+        if TestBankAccount.DEBUG:
+            print("\nTesting to see if the transfer is denied due to insufficient funds:")
+            
+        # bankAccount3 has $50.00
+        # The max withdraw is $50 + $250 = $300
+        transferAmount = 300.01 
+        
+        # Attempt to transfer money from bankAccount3 to bankAccount1
+        success = self.bankAccount1.tranfer(self.bankAccount3, transferAmount)
+        
+        # Assert that the transfer failed
+        #print("Trannsfer denied: ", success)
+        self.assertFalse(success)
+        
+        # Assert that bankAccount1's balance does not change
+        self.assertEqual(self.bankAccount1.balance, TestBankAccount.INITIAL_BALANCE)
+        
+        # Assert that bankAccount3's balance does not change (aka the withdrawal failed)
+        self.assertEqual(self.bankAccount3.balance, 50.00)
+
+
+    # Test the printTransactions method 
+    def testPrintTransactions(self):
+        # Print debug message if DEBUG is True
+        if TestBankAccount.DEBUG:
+            print("\nTesting printTransactions:")
+
+        # Record a deposit and a withdrawal to ensure the account has a transaction history in order to test the printTransactions method
+        self.bankAccount3.deposit(50.00)
+        self.bankAccount3.withdraw(20.00)
+
+        # Call the printTransactions method for bankAccount3
+        self.bankAccount3.printTransactions()
+
+        # Assert that the transactions list actually contains the recorded transactions
+        self.assertGreater(len(self.bankAccount3.transactions), 0)
+
+    
+    # test calculate interest 
+    def test_calculateInterest(self):
+        # creating the interest test
+        interestTest1 = self.account4.calculateInterest()
+        # checking that its equal to 3440 (the balance + the interest earned)
+        self.assertEqual(self.account4.balance, 3440)
+        
+        # printing the information if debug is true 
+        if TestBankAccount.debug:
+            print("\nTesting Calculate Interest")
+            print("First Account After Interest", self.account4.balance)
+           
+    # test deposit 
+    def testDeposit(self):
+           # Should work
+        self.assertTrue(self.acc1.deposit(500000)) 
+        self.assertEqual(self.acc1.balance, 1500000.0)
+        print(f" New account balance: ${self.acc1.balance:.2f}\n")
+           
+           # Should not work
+        self.assertFalse(self.acc1.deposit(-10)) 
+        self.assertEqual(self.acc1.balance, 1500000.0)
+        print(f" New account balance: ${self.acc1.balance:.2f}\n")
+        
+    # test withdrawl
+    def testWithdrawl(self):
+        # Should work and remove money from acc1
+        self.acc1.withdraw(500000)
+        self.assertEqual(self.acc1.balance, 500000.0)
+        print(f" New account balance: ${self.acc1.balance:.2f}\n")
+        
+        # Should be equal to the first withdrawl
+        self.acc1.withdraw(2000000)
+        self.assertEqual(self.acc1.balance, 500000)
+        print(f" New account balance: ${self.acc1.balance:.2f}\n")
+        
+    # test overdraft process 
+    def testOverdraft(self):
+        self.acc1.withdraw(1000001)
+        self.assertEqual(self.acc1.balance, -1.0)
+        if TestBankAccount.debug:
+            print("Testing overdraft")
+            
+        overdraft = self.acc1.getOverdraft()
+        balance = self.acc1.getBalance()
+        print("Amount of times of overdrafted: %d" % (overdraft))
+        print("New Balance: %d" % (balance))
+        
+    # testing special method equality    
+    def test_eq_(self):
+        if TestBankAccount.debug:
+            print("Testing equality")
+        self.assertTrue(self.account4 == self.account4)
+        self.assertFalse(self.account5 == self.account4)
+       
+    # testing special method inequality 
+    def test_neq(self):
+        if TestBankAccount.debug:
+            print("Testing inequality")
+        self.assertTrue(self.account4 != self.account5)
+        self.assertFalse(self.account5 != self.account5)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+        
---
+++ Project1/bankAccount.py
@@ -0,0 +1,218 @@
+# BankAccount.py
+# 
+# Authors: Hanna Nash, Victoria Seusankar, Will Wian
+# 
+# Date: 9/6/25
+
+# This module defines the BankAccount class
+
+from transaction import Transaction
+
+class BankAccount:
+  # Attributes (overdraft fee, interest rate, next available account number)
+  OVERDRAFT_FEE = 20.00
+  INTEREST_RATE = 0.075
+  _NEXTACCOUNTNUMBER = 1000
+  
+  
+
+  
+  # Constructs a bank account.
+  #@param firstName: The first name of the account holder
+  #@param lastName: The last name of the account holder
+  #@param initBalance: The initial bank account balance (float)
+  # @require: The length of the first name has to be in between 1 and 25
+  # @require: The length of the last name has to be in between 1 and 40
+  def __init__(self, firstName, lastName, initBalance=0.0):
+    assert firstName.isalpha and len(firstName) >= 1 and len(firstName) <= 25
+    assert lastName.isalpha and len(lastName) >= 1 and len(lastName) <= 40    
+    self.firstName = firstName
+    self.lastName = lastName
+    self.balance = float(initBalance)
+    self.accountNumber = BankAccount._NEXTACCOUNTNUMBER
+    BankAccount._NEXTACCOUNTNUMBER += 1
+    self.transactionNumber = 100
+    self.transactions = []
+    self.overdrawn = 0
+
+
+  # Returns a string that contains the account details (first & last name, account number, balance, overdrawn counter)
+  #@return A human readable string containing the account details
+  def __str__(self):
+    return (f"Account Holder: {self.firstName} {self.lastName}\n"
+                f"Account Number: {self.accountNumber}\n"
+                f"Balance: ${self.balance:.2f}\n")
+
+
+  # Deposit an amount into the account
+  #@param amount: The amount being deposited (float)
+  #@return True if the deposit is successful, and False otherwise
+  def deposit(self, amount):
+    if amount <= 0:
+      return False
+    else:
+      self.balance = self.balance + amount
+      balanceTransaction = Transaction(self.transactionNumber, "deposit", amount)
+      #self.transactions.append(f"Deposited ${amount:.2f}")
+      self.transactions.append(balanceTransaction)
+      self.transactionNumber += 1
+      return True
+
+
+  # Calculate the interest and add the interest amount to the account
+  #@return True of the interest is successfully calculated, and False otherwise
+  def calculateInterest(self):
+    # the interest earned on the balance based on the interest rate
+    interestEarned = self.balance * BankAccount.INTEREST_RATE
+    # adding the interest earned to the balance 
+    self.balance += interestEarned
+    # creating an interest transaction 
+    interestTransaction = Transaction(self.transactionNumber, "interest", interestEarned)
+    # appending the transaction to the transactions list
+    self.transactions.append(interestTransaction)
+    self.transactionNumber += 1
+    
+    return True
+
+  # Withdraw an amount from the account
+  #@param amount: The amount being withdrawn (float)
+  #@return True if the withdrawl is successful, and False otherwise
+  def withdraw(self, amount):
+    # creating a variable to check the balance + 250
+    withdrawCheck = self.balance + 250 
+    # checking if the amount is less than the withdraw check
+    if (amount > withdrawCheck):
+      print("Transaction denied\n")
+      return False
+    
+    # checking if that the balance is greater than zero
+    elif (self.balance > 0):
+      # creating the withdraw transaction 
+      withdrawTransacation = Transaction(self.transactionNumber, "withdrawal", amount)
+      # appending the withdraw to the transactions list 
+      self.transactions.append(withdrawTransacation)
+      
+      # adding to the transaction number
+      self.transactionNumber += 1
+      # subtracting the amount from the balance 
+      self.balance = self.balance - amount
+      
+      # checking if the balance is less than 0 
+      if (self.balance < 0):
+        # adding to the overdrawn counter
+        self.overdrawn += 1
+        
+        # creating a penalty transaction 
+        penaltyTransaction = Transaction(self.transactionNumber, "penalty", amount)
+        
+        # adding to the transaction number
+        self.transactionNumber += 1
+        # subtract from the balance with the overdraft fee
+        overdrawnDeduct = self.balance - BankAccount.OVERDRAFT_FEE
+        
+        print("Account has been overdrawn\n")
+        return False
+      
+      else: 
+        print("Transaction Complete")
+        return True
+
+    else:
+      print("Transaction has been denied\n")
+      return False
+      
+
+
+  # Transfer an amount to this account from the account passed as a parameter
+  #@param fromAccount: The BankAccount object to transfer the money from
+  #@param amount: The amount being transfered
+  #@return True if the transfer is successful, and False otherwise
+  def tranfer(self, fromAccount, amount):
+    # If fromAccount tries to make a transfer to itself, deny the transfer and return false
+    if fromAccount is self:
+      print("Transfer denied: Cannot transfer to the same account.")
+      return False
+
+    # If the amount being transfered is negative or equal to 0, print a message to let the user know
+    # that the amount being transferred needs to be positive, and return false
+    if amount <= 0:
+      print("Transfer denied: Amount must be positive.")
+      return False
+
+    # Create a variable to store the successful withdrawal
+    successfulWithdrawal = fromAccount.withdraw(amount)
+
+    # If the withdrawal is successful, deposit the specified amount, and return true
+    if successfulWithdrawal:
+      self.deposit(amount) 
+      return True
+
+    # If the withdrawal does not meet the proper conditions, print a message to let the user know
+    # that the transfer failed, and return false
+    else:
+      print("Transfer failed: Withdrawal from the source account was denied.")
+      return False
+
+
+
+  # Print the list of transactions for an account
+  def printTransactions(self):
+    print("\n Transactions for Account %d" % (self.accountNumber))
+
+    # Check if the list of transactions is empty
+    if not self.transactions:
+      print("No transactions recorded.")
+      return
+
+    # Iterate through the list of transactions and print them
+    for transaction in self.transactions:
+      print(transaction)
+      
+  # initializing the getters 
+  def getFirst(self):
+      return self.firstName
+    
+  def getLast(self):
+      return self.lastName 
+      
+  def getAccountNumber(self):
+      return self.accountNumber  
+   
+  def getBalance(self):
+      return self.balance 
+      
+  def getTransactions(self):
+      return self.transactions 
+      
+  def getBalance(self):
+      return self.balance   
+  
+  def getOverdraft(self):
+      return self.overdrawn
+    
+  # initalizing the setters
+  def setFirst(self, first):
+      self.firstName = first
+  
+  def setLast(self, last):
+      self.lastName = last
+      
+  # creating the string representation 
+  def __str__(self):
+    return ("Account Holder: %s %s Account Number: %d Balance: %0.2f" % (self.firstName, self.lastName, self.accountNumber, self.balance))
+  
+  # creating the machine representation
+  def __repr__(self):
+    return ("BankAccount(firstName = %s, lastName = %s, balance = %d)" % self.firstName, self.lastName, self.balance)
+  
+  # creating the special methods (equality and non-equality)
+  def __eq__(self, other):
+    result = (self.firstName == other.firstName) and (self.lastName == other.lastName) and (self.balance == other.balance)
+    return result
+  
+  def __ne__(self, other):
+    result = (self.firstName != other.firstName) or (self.lastName != other.lastName) or (self.balance != other.balance)
+    return result
+  
+  
+
