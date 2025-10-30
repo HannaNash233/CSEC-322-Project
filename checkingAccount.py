2989c860cdee9bb79bcb18dd3a032b1375702b89
HannaNash233 <145599518+HannaNash233@users.noreply.github.com>
2025-10-29 21:18:29 -0400

    added checkingAccount, savingsAccount, and client

---
+++ checkingAccount.py
@@ -0,0 +1,36 @@
+# checkingAccount.py
+#
+# authors: Will Wian, Detric Brown, Evan Fannin
+#
+# date: 10/29/25
+
+class CheckingAccount:
+    
+    def __init__(self, initBalance=0.0):
+        pass
+    
+    def displayDetails(self):
+        pass
+    
+    def deposit(self, amount):
+        pass
+    
+    def calculateInterest(self):
+        pass
+    
+    def withdraw(self, amount):
+        pass
+    
+    def transfer(self, fromAccount, amount):
+        pass
+    
+    def printTransactions(self):
+        pass
+    
+    def writeTransactions(self, filename):
+        pass
+    
+    def readTransactions(self, filename):
+        pass
+    
+    
---
+++ savingsAccount.py
@@ -0,0 +1,34 @@
+# savingsAccount.py
+# 
+# authors: Hanna Nash, Victoria Seusankar, Will Wian
+#
+# Date: 10/29/25
+
+class SavingsAccount:
+    
+    def __init__(self, initBalance=0.0):
+        pass
+    
+    def displayDetails(self):
+        pass
+    
+    def deposit(self, amount):
+        pass
+    
+    def calculateInterest(self):
+        pass
+    
+    def withdraw(self, amount):
+        pass
+    
+    def transfer(self, fromAccount, amount):
+        pass
+    
+    def printTransactions(self):
+        pass
+    
+    def writeTransactions(self, filename):
+        pass
+    
+    def readTransactions(self, filename):
+        pass    
