18174fd2329f1f797624bce7f352d9d80036f1a8
Efannin30352 <169088493+Efannin30352@users.noreply.github.com>
2025-10-30 11:33:28 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,42 @@
+# client.py
+#
+# Authors: Detric Brown, Evan Fannin, Hanna Nash, Victoria Seusankar
+#
+# Date: 10/29/25
+
+class Client:
+    
+    def __init__(self, firstName, lastName, phoneNum, address):
+        pass
+    
+    def getClientNumber(self):
+        pass
+    
+    def getFirstName(self):
+        pass
+    
+    def getAddress(self):
+        pass
+    
+    def getPhoneNum(self):
+        pass
+    
+    def getAccountType(self):
+        pass
+    
+    def openAccount(self, typeAccount):
+        pass
+    
+    def closeAccount(self, typeAccount):
+        pass
+    
+    def setFirst(self, firstName):
+        pass
+    
+    def setAddress(self, address):
+        pass
+    
+    def setPhoneNum(self, phoneNum):
+        pass 
+
+
