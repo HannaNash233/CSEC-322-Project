f04745f54024e3085f1e1c9a72f45e18ba90dd72
Efannin30352 <169088493+Efannin30352@users.noreply.github.com>
2025-11-05 08:46:06 -0500

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,9 @@
+def setUp(self):
+  self.bankAccount1 = BankAccount(savings)
+  self.bankAccount2 = BankAccount(checkings)
+  self.bankAccount3 = BankAccount(forFun)
+
+def TestopenAccount():
+  self.assertEqual(getAccountType(self),bankAccount1)
+  self.assertEqual(getAccountType(self),bankAccount1)
+  self.assertEqual(getAccountType(self),bankAccount1)
