21f06eef932e8069837cb446daa94ace6a736576
Efannin30352 <169088493+Efannin30352@users.noreply.github.com>
2025-11-05 08:55:13 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -3,7 +3,50 @@ def setUp(self):
   self.bankAccount2 = BankAccount(checkings)
   self.bankAccount3 = BankAccount(forFun)
 
-def TestopenAccount():
+def TestOpenAccount(self):
   self.assertEqual(getAccountType(self),bankAccount1)
-  self.assertEqual(getAccountType(self),bankAccount1)
-  self.assertEqual(getAccountType(self),bankAccount1)
+  self.assertEqual(getAccountType(self),bankAccount2)
+  self.assertEqual(getAccountType(self),bankAccount3)
+
+
+
+def TestCloseAccount(self):
+  the
+
+
+
+
+
+
+
+class SavingsAccount (BankAccount): 
+  
+  INTEREST_RATE = 0.04
+  # creating the constructor
+  def __init__(self, firstName, lastName, initBalance=0.0, bType="Savings"):
+    # inheriting from BankAccount class
+    super().__init__(firstName, lastName, initBalance)
+    self.type = bType  
+        
+    
+  def displayDetails(self):
+    details = ("First Name: %s \n Last Name: %s \n Balance: %0.2f\n Type: %s" % (self.firstName, self.lastName, self.balance, self.type))
+    return details  
+
+
+
+def TestInit(self):
+  the
+
+
+def TestDisplayDetails(self):
+  the
+
+def TestDeposit():
+  the
+  
+
+
+
+
+
