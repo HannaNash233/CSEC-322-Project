887e730016b3e04db0d6d4348702d4a53b369974
detricb1 <detricbrown@go.rmc.edu>
2025-11-05 08:37:13 -0500

    Create checkingAccountTester.py

---
+++ checkingAccountTester.py
@@ -0,0 +1,27 @@
+# checkingAccountTester.py
+#
+# Authors:
+#
+# Date: 11/5/25
+#
+# This is the testing module for the checking account class.
+
+import unittest
+from checkingAccount import CheckingAccount
+
+class testCheckingAccount(unittest.TestCase):
+
+
+
+    def setUp(self):
+      pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
