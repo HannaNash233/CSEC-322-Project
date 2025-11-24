172e3c3f2b1be3feeec5b9eda6f6892837d3c9fd
victoriaseusankar <victoriaseusankar@go.rmc.edu>
2025-11-24 08:54:40 -0500

    Update passwordValidator.py

--- passwordValidator.py
+++ passwordValidator.py
@@ -1,1 +1,28 @@
+# passwordValidator.py
+# 
+# Authors: Victoria Seusankar, ...
+# 
+# Date: 11/24/25
+
+
+class passwordValidator:
+
+  # Constructor
+  def __init__():
+    pass
+
+
+  # Checks to see if a password is valid
+  def isValid(password: str):
+    pass
+
+
+  # Hashes a password
+  def hash(password: str, salt: bytes = None):
+    pass
+
+
+  # Verifies a password
+  def verify(password: str, stored_hash: bytes, salt: bytes):
+    pass
 
