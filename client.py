fb7162152a936f40187cb2bf44337eb753bcad6a
Efannin30352 <169088493+Efannin30352@users.noreply.github.com>
2025-11-24 09:18:39 -0500

    Update client.py

--- client.py
+++ client.py
@@ -27,21 +27,10 @@ class Client:
     # @require: state is in VALID_STATES
     # @ensure Client receives a unique client number and one account of chosen type
     
-    def __init__(self, firstName, lastName, phoneNum, street, city, state):
-        assert firstName.isalpha() and 1 <= len(firstName) <= 25
-        assert lastName.isalpha() and 1 <= len(lastName) <= 40
-        assert phoneNum.isdigit() and len(phoneNum) == 10 and phoneNum[0] not in "012"
-        assert isinstance(phoneNum, str)
-
-       # street, city, state = address
-        address = street, city, state
-        assert len(street) <= 30
-        assert len(city) <= 30
-        assert state in Client.VALID_STATES
-        #assert street and len(street) <= 30 and street.replace(" ","").isalnum()
-        #assert city and len(city) <= 30 and city.replace(" ","").isalnum()
-        #assert state in Client.VALID_STATES
-        #assert accountType in {"Checking", "Savings"}, "Account type must be 'Checking' or 'Savings'"
+    def __init__(self, name, phoneNum,address):
+    	assert(isinstance(name,Name)), "Invalid Name"
+    	assert(isinstance(phoneNum,PhoneNumber)), "Invalid Phone Number"
+    	assert(isinstance(address,ClientAddress)), "Invalid Address"
         
         self._firstName = firstName
         self._lastName = lastName
@@ -124,3 +113,4 @@ class Client:
         
     
   
+
