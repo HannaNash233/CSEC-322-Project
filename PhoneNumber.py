8717c0be0fe3abf20f116a3f4a973020a9a476fc
victoriaseusankar <victoriaseusankar@go.rmc.edu>
2025-12-13 17:26:25 -0500

    Update PhoneNumber.py

--- PhoneNumber.py
+++ PhoneNumber.py
@@ -1,35 +1,28 @@
-
-
 class PhoneNumber:
-
+    
     def __init__(self, phoneNum):
-
-    	assert phoneNum.isdigit() and len(phoneNum) == 10 and phoneNum[0] not in "012"
-        
+        assert phoneNum.isdigit() and len(phoneNum) == 10 and phoneNum[0] not in "012"
         self._phoneNum = phoneNum
         
-        
-          # creating the string representation 
-      def __str__(self):
+    # creating the string representation 
+    def __str__(self):
         return ("Phone Number: %d " % (self.phoneNum))
         	
-        	
-      def __eq__(self, other):
+    def __eq__(self, other):
         result = (self.phoneNum == other.phoneNum) 
-       	return result
-       	
-  
-  		def __gt__(self, other):
-  			result =  (self.phoneNum > other.phoneNum)
-  			return result  
-  			
-  		def __ge__(self, other):
-  			result = (self.phoneNum >= other.phoneNum)
-  			return result
+        return result
+        
+    def __gt__(self, other):
+  		result =  (self.phoneNum > other.phoneNum)
+  		return result
+        
+    def __ge__(self, other):
+  		result = (self.phoneNum >= other.phoneNum)
+  		return result
 
 
-        def getPhoneNum(self):
-  			return self.phoneNum
+    def getPhoneNum(self):
+  		return self.phoneNum
   			
-  		def _setPhoneNum(self, phoneNum):
-  			self._phoneNum = phoneNum
+  	def _setPhoneNum(self, phoneNum):
+  		self._phoneNum = phoneNum
